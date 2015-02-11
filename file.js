fa82bf633c784311fd5b9c79eba4056065ec0967
Arnout Kazemier <info@3rd-Eden.com>
2015-02-11 22:52:13 +0100

    [major] Work on processing flow

--- file.js
+++ file.js
@@ -3,7 +3,9 @@
 var debug = require('diagnostics')('fever:file')
   , EventEmitter = require('eventemitter3')
   , hash = require('crypto').createHash
-  , path = require('path');
+  , sm = require('source-map')
+  , path = require('path')
+  , fs = require('fs');
 
 /**
  * Representation of a single file.
@@ -12,11 +14,11 @@ var debug = require('diagnostics')('fever:file')
  *
  * @constructor
  * @param {Fever} fever The fever that creates and manages the files.
- * @param {String} path Location of the file.
+ * @param {String} file Location of the file.
  * @param {Object} options File configuration.
  * @api public
  */
-function File(fever, path, options) {
+function File(fever, file, options) {
   if (!this) return new File(path, options);
   options = options || {};
 
@@ -24,9 +26,10 @@ function File(fever, path, options) {
   this.contents = [];           // Various of file that we should read.
   this.fever = fever;           // Reference to the fever instance.
   this.alias = '';              // Alias of the file, also known as fingerprint.
+  this.smg = null;              // Placeholder for the source file generator.
 
   fever.emit('add', this);
-  if (path) this.push(path);
+  if (path) this.push(file);
 }
 
 //
@@ -37,19 +40,9 @@ Object.keys(EventEmitter.prototype).forEach(function each(key) {
   File.prototype[key] = EventEmitter.prototype[key];
 });
 
-/**
- * Generate a fingerprint of the current content.
- *
- * @param {String|Buffer} content Content that needs to be fingered.
- * @returns {File}
- * @api private
- */
-File.prototype.fingerprinter = function fingerprinter(content) {
-  this.alias = hash('md5').update(content.toString()).digest('hex');
-
-  return this;
-};
-
+//
+// Generate API methods which allows the adding and removing of file's.
+//
 ['push', 'shift', 'pop', 'unshift'].forEach(function generate(method) {
   File.prototype[method] = function compiled(path, options) {
     this.contents[method](path);
@@ -58,6 +51,29 @@ File.prototype.fingerprinter = function fingerprinter(content) {
     return this.modified();
   };
 });
+
+/**
+ * Generate a fingerprint based on all the contents.
+ *
+ * @param {Array} contents Array of read file contents.
+ * @returns {File}
+ * @api private
+ */
+File.prototype.fingerprinter = function fingerprinter(contents) {
+  var md5 = hash('md5');
+
+  //
+  // As crypto is actually a stream we can do multiple update calls with each of
+  // the files that we've stored in our contents array.
+  //
+  (contents || this.contents).forEach(function each(content) {
+    md5.update(content.toString());
+  });
+
+  this.alias = md5.digest('hex');
+
+  return this;
+};
 
 /**
  * Concatenate multiple files together.
@@ -100,13 +116,41 @@ File.prototype.buffer = function buffer(fn) {
 
 /**
  * Something in this file has been modified, we need to re-calculate all the
- * things.
+ * things:
+ *
+ * - Source map.
+ * - New alias based on the content.
  *
  * @returns {File}
  * @api public
  */
 File.prototype.modified = function modified() {
-  this.fingerprinter();
+  var contents = this.contents.map(function map(file) {
+    return {
+      content: fs.readFileSync(__dirname),
+      path: file
+    };
+  });
+
+  //
+  // Generate a new alias, as it only works file actual file content we need to
+  // return an array which only holds these contents.
+  //
+  this.fingerprinter(contents.map(function map(file) {
+    return file.content;
+  }));
+
+  //
+  // Now that we know the file's alias we can generate a new sourcemap.
+  //
+  this.smg = new sm.SourceMapGenerator({
+    file: this.alias
+  });
+
+  contents.forEach(function each(file) {
+    // @TODO check if we need to add an exiting file map.
+    this.smg.setSourceContent(file.path, file.content.toString());
+  }, this);
 
   return this;
 };
